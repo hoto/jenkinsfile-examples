e9aed66dde8d6af1a45e0bd7a247659f3c89454a
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2019-05-27 14:30:27 +0100

    Accessing dockerCert

--- jenkinsfiles/130-accessing-credentials.groovy
+++ jenkinsfiles/130-accessing-credentials.groovy
@@ -72,5 +72,24 @@ pipeline {
         }
       }
     }
+
+    stage('dockerCert') {
+      steps {
+        script {
+          withCredentials([
+            dockerCert(
+              credentialsId: 'production-docker-ee-certificate',
+              variable: 'DOCKER_CERT_PATH')
+          ]) {
+            print 'DOCKER_CERT_PATH=' + DOCKER_CERT_PATH
+            print 'DOCKER_CERT_PATH.collect { it }=' + DOCKER_CERT_PATH.collect { it }
+            print 'DOCKER_CERT_PATH/ca.pem=' + readFile("$DOCKER_CERT_PATH/ca.pem")
+            print 'DOCKER_CERT_PATH/cert.pem=' + readFile("$DOCKER_CERT_PATH/cert.pem")
+            print 'DOCKER_CERT_PATH/key.pem=' + readFile("$DOCKER_CERT_PATH/key.pem")
+          }
+        }
+      }
+    }
+
   }
 }
