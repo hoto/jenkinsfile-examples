ebefb043bf5a046e8e49e4852f77662188dd2f7b
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2019-05-27 20:09:24 +0100

    Listing credentials ids

--- jenkinsfiles/130-accessing-credentials.groovy
+++ jenkinsfiles/130-accessing-credentials.groovy
@@ -91,5 +91,13 @@ pipeline {
       }
     }
 
+    stage('list credentials ids') {
+      steps {
+        script {
+          sh 'cat $JENKINS_HOME/credentials.xml | grep "<id>"'
+        }
+      }
+    }
+
   }
 }
