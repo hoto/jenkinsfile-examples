bb69dd888c6c3fdd807a52ae6ffa01348d4f531d
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2019-05-27 14:03:16 +0100

    Accessing sshUserPrivateKey

--- jenkinsfiles/130-accessing-credentials.groovy
+++ jenkinsfiles/130-accessing-credentials.groovy
@@ -1,3 +1,5 @@
+// docs: https://jenkins.io/doc/pipeline/steps/credentials-binding/
+
 pipeline {
   agent any
   stages {
@@ -44,6 +46,28 @@ pipeline {
           ]) {
             print 'joke=' + joke
             print 'joke.collect { it }=' + joke.collect { it }
+          }
+        }
+      }
+    }
+
+    stage('sshUserPrivateKey') {
+      steps {
+        script {
+          withCredentials([
+            sshUserPrivateKey(
+              credentialsId: 'production-bastion',
+              keyFileVariable: 'keyFile',
+              passphraseVariable: 'passphrase',
+              usernameVariable: 'username')
+          ]) {
+            print 'keyFile=' + keyFile
+            print 'passphrase=' + passphrase
+            print 'username=' + username
+            print 'keyFile.collect { it }=' + keyFile.collect { it }
+            print 'passphrase.collect { it }=' + passphrase.collect { it }
+            print 'username.collect { it }=' + username.collect { it }
+            print 'keyFileContent=' + readFile(keyFile)
           }
         }
       }
