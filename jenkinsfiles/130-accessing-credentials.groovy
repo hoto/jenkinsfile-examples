68536723605c7b7e69629553aeedbe4a6ea94ee7
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2019-05-27 13:48:50 +0100

    Accessing string (secret text)

--- jenkinsfiles/130-accessing-credentials.groovy
+++ jenkinsfiles/130-accessing-credentials.groovy
@@ -10,14 +10,10 @@ pipeline {
               usernameVariable: 'username',
               passwordVariable: 'password')
           ]) {
-            print 'username, password='
-            print username + password
+            print 'username=' + username + 'password=' + password
 
-            print 'username.collect { it }='
-            print username.collect { it }
-
-            print 'password.collect { it }='
-            print password.collect { it }
+            print 'username.collect { it }=' + username.collect { it }
+            print 'password.collect { it }=' + password.collect { it }
           }
         }
       }
@@ -31,11 +27,23 @@ pipeline {
               credentialsId: 'gitlab',
               variable: 'userpass')
           ]) {
-            print 'userpass='
-            print userpass
+            print 'userpass=' + userpass
+            print 'userpass.collect { it }=' + userpass.collect { it }
+          }
+        }
+      }
+    }
 
-            print 'userpass.collect { it }='
-            print userpass.collect { it }
+    stage('string (secret text)') {
+      steps {
+        script {
+          withCredentials([
+            string(
+              credentialsId: 'joke-of-the-day',
+              variable: 'joke')
+          ]) {
+            print 'joke=' + joke
+            print 'joke.collect { it }=' + joke.collect { it }
           }
         }
       }
